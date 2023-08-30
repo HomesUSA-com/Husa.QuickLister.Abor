ef1f1518f565e3999a23a34a03dc2eafd71e006e
Miguel Muñoz <joguel63@gmail.com>
2023-08-30 14:56:10 +0000

    Merged in bugfix/HRP-4400 (pull request #50)
    HRP-4400 Changed enum members
    
    * Changed enum members
    
    * Uodated client
    
    
    Approved-by: Gonzalo Rodulfo
    Approved-by: Andrea Centeno
    Approved-by: Wladimir

--- Husa.Quicklister.Abor.Domain.Enums/Domain/WindowFeatures.cs
+++ Husa.Quicklister.Abor.Domain.Enums/Domain/WindowFeatures.cs
@@ -5,37 +5,37 @@ namespace Husa.Quicklister.Abor.Domain.Enums.Domain
 
     public enum WindowFeatures
     {
-        [EnumMember(Value = "BayWindows")]
+        [EnumMember(Value = "BWND")]
         [Description("Bay Window(s)")]
         BayWindows,
         [EnumMember(Value = "Blinds")]
         [Description("Blinds")]
         Blinds,
-        [EnumMember(Value = "DoublePaneWindows")]
+        [EnumMember(Value = "DPWND")]
         [Description("Double Pane Windows")]
         DoublePaneWindows,
-        [EnumMember(Value = "ENERGYSTARQualifiedWindows")]
+        [EnumMember(Value = "EQWIN")]
         [Description("ENERGYSTAR Qualified Windows")]
         EnergyStarQualifiedWindows,
-        [EnumMember(Value = "InsulatedWindows")]
+        [EnumMember(Value = "INWND")]
         [Description("Insulated Windows")]
         InsulatedWindows,
-        [EnumMember(Value = "none")]
+        [EnumMember(Value = "None")]
         [Description("None")]
         None,
-        [EnumMember(Value = "PlantationShutters")]
+        [EnumMember(Value = "PSHUT")]
         [Description("Plantation Shutters")]
         PlantationShutters,
-        [EnumMember(Value = "Screens")]
+        [EnumMember(Value = "SCR")]
         [Description("Screens")]
         Screens,
-        [EnumMember(Value = "Shutters ")]
+        [EnumMember(Value = "SHTRS")]
         [Description("Shutters")]
         Shutters,
-        [EnumMember(Value = "StormWindows")]
+        [EnumMember(Value = "STWND")]
         [Description("Storm Windows")]
         StormWindows,
-        [EnumMember(Value = "Vinyls")]
+        [EnumMember(Value = "VWNDW")]
         [Description("Vinyl Windows")]
         VinylWindows,
     }
