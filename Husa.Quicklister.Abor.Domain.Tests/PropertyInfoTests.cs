90045ba885c8a62fa47240b9eb762b1295c61736
Israel Marquez <imarquez@homesusa.com>
2024-03-11 22:40:53 +0000

    Merged in feature/HRP-5960 (pull request #397)
    HRP-5960 construction stage validation
    
    * HRP-5960 construction stage validation
    
    * HRP-5960 test added
    
    
    Approved-by: Andrea Centeno
    Approved-by: Isaac Gonzalez

---
+++ Husa.Quicklister.Abor.Domain.Tests/PropertyInfoTests.cs
@@ -0,0 +1,40 @@
+namespace Husa.Quicklister.Abor.Domain.Tests
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using Husa.Quicklister.Abor.Domain.Entities.Listing;
+    using Husa.Xml.Api.Contracts.Response;
+    using Xunit;
+
+    [ExcludeFromCodeCoverage]
+    [Collection("Husa.Husa.Quicklister.Abor.Domain.Tests")]
+    public class PropertyInfoTests
+    {
+        [Theory]
+        [InlineData("-1")]
+        [InlineData("+1")]
+        public void ImportFromPropertyInfoFromXmlSuccess(string offsetString)
+        {
+            int offset = int.Parse(offsetString);
+            var dateInput = DateTime.Today.AddDays(offset).Date;
+            var propertyInfo = new PropertyInfo();
+
+            // Arrange
+            var xmlLsitng = new XmlListingDetailResponse
+            {
+                Latitude = default(decimal),
+                Longitude = default(decimal),
+                Day = dateInput,
+                LegalDescLot = "legal desc",
+            };
+
+            // Act
+            var featuresClonned = PropertyInfo.ImportFromXml(xmlLsitng, propertyInfo);
+
+            // Assert
+            Assert.Equal(xmlLsitng.Day.Value.Date, featuresClonned.ConstructionCompletionDate.Value.Date);
+            var constructionStage = xmlLsitng.Day.Value.Date > DateTime.UtcNow.Date ? Enums.Domain.ConstructionStage.Incomplete : Enums.Domain.ConstructionStage.Complete;
+            Assert.Equal(featuresClonned.ConstructionStage, constructionStage);
+        }
+    }
+}
