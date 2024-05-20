292943b3e7683e7628b9c8280f8a3a1496a34126
IsraelMarquez <imarquez@homesusa.com>
2024-05-20 14:34:52 -0400

    HRP-6290 Update from extensions

---
+++ Husa.Quicklister.Abor.Application/Interfaces/Media/IVirtualTourService.cs
@@ -0,0 +1,7 @@
+namespace Husa.Quicklister.Abor.Application.Interfaces.Media
+{
+    using Extensions = Husa.Quicklister.Extensions.Application.Interfaces;
+    public interface IVirtualTourService : Extensions.Media.IVirtualTourService
+    {
+    }
+}
