848bac3932dceb177f20aa69d57018bbe1ffd012
Wladimir-husa <wladimir@homesusa.com>
2024-06-06 14:24:55 -0500

    removed validation of contract

--- Husa.Quicklister.Abor.Api.Contracts/Request/ListingRequest.cs
+++ Husa.Quicklister.Abor.Api.Contracts/Request/ListingRequest.cs
@@ -8,7 +8,7 @@ namespace Husa.Quicklister.Abor.Api.Contracts.Request
     {
         public Guid Id { get; set; }
 
-        public virtual decimal? ListPrice { get; set; }
+        public decimal? ListPrice { get; set; }
 
         public DateTime? ExpirationDate { get; set; }
 
