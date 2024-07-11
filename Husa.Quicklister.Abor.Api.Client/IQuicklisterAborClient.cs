e41a09012e4f977b265e23f88b130b3b0f3c892e
andy_nunez <andy@homesusa.com>
2024-07-11 13:27:43 -0400

    HRP-6529: Add ListingLotRequest to API client and Update Quicklister.Extensions.Api.Client to support Uploader Lot Requests.

--- Husa.Quicklister.Abor.Api.Client/IQuicklisterAborClient.cs
+++ Husa.Quicklister.Abor.Api.Client/IQuicklisterAborClient.cs
@@ -1,13 +1,17 @@
 namespace Husa.Quicklister.Abor.Api.Client
 {
     using Husa.Quicklister.Abor.Api.Client.Interfaces;
+    using Husa.Quicklister.Abor.Api.Client.Interfaces.LotListing;
     using Husa.Quicklister.Abor.Api.Contracts.Response;
+    using Husa.Quicklister.Abor.Api.Contracts.Response.ListingRequest.LotRequest;
     using Husa.Quicklister.Abor.Api.Contracts.Response.ListingRequest.SaleRequest;
     using Husa.Quicklister.Extensions.Api.Client;
+    using Husa.Quicklister.Extensions.Api.Client.LotListing;
 
     public interface IQuicklisterAborClient :
         IQuicklisterCommunityClient<ISaleCommunity>,
         IQuicklisterListingRequestClient<IListingSaleRequest, ListingSaleRequestQueryResponse, ListingSaleRequestDetailResponse>,
+        IQuicklisterListingLotRequestClient<IListingLotRequest, ListingLotRequestQueryResponse, LotListingRequestDetailResponse>,
         IQuicklisterAlertClient<IAlert, AlertDetailResponse>
     {
         public ISaleListing SaleListing { get; }
