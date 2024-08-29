12ac2eb02931e23e6e98b9f4617287f9579c0d8c
Andrea <andrea@homesusa.com>
2024-08-28 22:12:37 -0300

    HRP-6823 update client alert resource

--- Husa.Quicklister.Abor.Api.Client/Interfaces/IAlert.cs
+++
@@ -1,9 +0,0 @@
-namespace Husa.Quicklister.Abor.Api.Client.Interfaces
-{
-    using Husa.Quicklister.Abor.Api.Contracts.Response;
-    using QlExtension = Husa.Quicklister.Extensions.Api.Client.Interfaces;
-
-    public interface IAlert : QlExtension.IAlert<AlertDetailResponse>
-    {
-    }
-}
