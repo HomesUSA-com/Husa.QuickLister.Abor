857a1317f4ad0c004964e376b4a1b760e7f2d380
José León <jleon@homesusa.com>
2024-09-18 17:20:07 +0000

    Merged in hotfix/2.14.2 (pull request #726)
    HRP-6885 Implementation of GetListingDetails in CallForwardService
    
    * HRP-6885 Implementation of GetListingDetails in CallForwardService
    
    * HRP-6885 Add null check for listing object
    
    * HRP-6885 Add using system
    
    
    Approved-by: Andrea Centeno
    Approved-by: Israel Marquez

--- Husa.Quicklister.Abor.Application/Services/SaleListings/CallForwardService.cs
+++ Husa.Quicklister.Abor.Application/Services/SaleListings/CallForwardService.cs
@@ -1,11 +1,14 @@
 namespace Husa.Quicklister.Abor.Application.Services.SaleListings
 {
+    using System;
     using Husa.CompanyServicesManager.Api.Client.Interfaces;
     using Husa.CompanyServicesManager.Api.Contracts.Response;
     using Husa.Extensions.Authorization;
+    using Husa.Extensions.Common;
     using Husa.Quicklister.Abor.Domain.Entities.Listing;
     using Husa.Quicklister.Abor.Domain.Enums;
     using Husa.Quicklister.Abor.Domain.Repositories;
+    using Husa.Quicklister.Extensions.Application.Models.Listing;
     using Microsoft.Extensions.Logging;
     using QLExtensions = Husa.Quicklister.Extensions.Application.Services.SaleListings;
     public class CallForwardService : QLExtensions.CallForwardService<SaleListing, IListingSaleRepository>
@@ -46,5 +49,21 @@ namespace Husa.Quicklister.Abor.Application.Services.SaleListings
                 ListType.Lots => (company.PhoneLeadInfo.IsCentralizedForLot, company.PhoneLeadInfo.CentralizeLeadPhoneForLot),
                 _ => (false, string.Empty),
             };
+
+        protected override ListingDetails GetListingDetails(SaleListing listing)
+        {
+            if (listing == null)
+            {
+                throw new ArgumentNullException(nameof(listing));
+            }
+
+            return new ListingDetails()
+            {
+                Address = listing.SaleProperty.Address,
+                City = listing.SaleProperty.AddressInfo.City.GetEnumDescription(),
+                ZipCode = listing.SaleProperty.AddressInfo.ZipCode,
+                Subdivision = listing.SaleProperty.AddressInfo.Subdivision,
+            };
+        }
     }
 }
