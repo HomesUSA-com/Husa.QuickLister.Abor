32b6f87d4625a14b9e7f3be28b5d3b016dbc8d3b
Andrea Centeno <andrea@homesusa.com>
2025-06-26 12:24:04 +0000

    Merged in feature/HRP-9271 (pull request #1145)
    HRP-9271 listing saved changes log
    
    * HRP-9271 listing saved changes log
    
    
    Approved-by: Israel Marquez
    Approved-by: Wladimir
    Approved-by: gonzalo

---
+++ Husa.Quicklister.Abor.Data.Commands/Extensions/ListingChangesLogExtensions.cs
@@ -0,0 +1,65 @@
+namespace Husa.Quicklister.Abor.Data.Commands.Extensions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Husa.Extensions.Document.Extensions;
+    using Husa.Extensions.Document.ValueObjects;
+    using Husa.Quicklister.Abor.Domain.Comparers;
+    using Husa.Quicklister.Abor.Domain.Entities.Listing;
+    using Husa.Quicklister.Abor.Domain.Entities.Property;
+    using Husa.Quicklister.Abor.Domain.Interfaces;
+
+    public static class ListingChangesLogExtensions
+    {
+        public static void ProcessEntityChanges(List<SummaryField> fields, SaleListing originalEntity, SaleListing updatedEntity)
+        {
+            var entityPairs = new Dictionary<string, (object Original, object Updated)>
+                {
+                    { nameof(SaleListing.StatusFieldsInfo), (originalEntity.StatusFieldsInfo, updatedEntity.StatusFieldsInfo) },
+                    { nameof(SaleListing.AdditionalInstructions), (originalEntity.AdditionalInstructions, updatedEntity.AdditionalInstructions) },
+                    { nameof(SaleListing.AppointmentRestrictions), (originalEntity.AppointmentRestrictions, updatedEntity.AppointmentRestrictions) },
+                    { nameof(SaleListing.AccessInformation), (originalEntity.AccessInformation, updatedEntity.AccessInformation) },
+                    { nameof(SaleListing.InvoiceInfo), (originalEntity.InvoiceInfo, updatedEntity.InvoiceInfo) },
+                    { nameof(SaleListing.PublishInfo), (originalEntity.PublishInfo, updatedEntity.PublishInfo) },
+                };
+            fields.AddSectionProperties(entityPairs);
+
+            var ignoreRootProperties = entityPairs.Keys.Concat([nameof(SaleListing.SaleProperty)]);
+            fields.AddProperties(
+                originalEntity,
+                updatedEntity,
+                excludeFields: ignoreRootProperties.ToArray());
+        }
+
+        public static void ProcessSalePropertyChanges(List<SummaryField> fields, SaleProperty originalProperty, SaleProperty updatedProperty)
+        {
+            var salePropertyPrefix = $"{nameof(SaleListing.SaleProperty)}.";
+            fields.AddProperties(
+                originalProperty,
+                updatedProperty,
+                salePropertyPrefix,
+                filterFields: [nameof(SaleProperty.CommunityId), nameof(SaleProperty.OwnerName), nameof(SaleProperty.PlanId)]);
+            var propertyPairs = new Dictionary<string, (object Original, object Updated)>
+                {
+                    { nameof(SaleProperty.AddressInfo), (originalProperty?.AddressInfo, updatedProperty.AddressInfo) },
+                    { nameof(SaleProperty.PropertyInfo), (originalProperty?.PropertyInfo, updatedProperty.PropertyInfo) },
+                    { nameof(SaleProperty.ShowingInfo), (originalProperty?.ShowingInfo, updatedProperty.ShowingInfo) },
+                    { nameof(SaleProperty.SchoolsInfo), (originalProperty?.SchoolsInfo, updatedProperty.SchoolsInfo) },
+                    { nameof(SaleProperty.FeaturesInfo), (originalProperty?.FeaturesInfo, updatedProperty.FeaturesInfo) },
+                    { nameof(SaleProperty.FinancialInfo), (originalProperty?.FinancialInfo, updatedProperty.FinancialInfo) },
+                    { nameof(SaleProperty.SpacesDimensionsInfo), (originalProperty?.SpacesDimensionsInfo, updatedProperty.SpacesDimensionsInfo) },
+                };
+            fields.AddSectionProperties(propertyPairs, salePropertyPrefix);
+            fields.AddPropertiesWithComparer<ListingSaleRoom, ListingRoomComparer>(
+                updatedProperty.Rooms,
+                originalProperty?.Rooms,
+                $"{salePropertyPrefix}Rooms",
+                filterFields: typeof(IProvideRoomInfo).GetProperties().Select(p => p.Name).ToArray());
+            fields.AddPropertiesWithComparer<SaleListingOpenHouse, OpenHouseComparer>(
+                updatedProperty.OpenHouses,
+                originalProperty?.OpenHouses,
+                $"{salePropertyPrefix}OpenHouses",
+                filterFields: typeof(IProvideOpenHouseInfo).GetProperties().Select(p => p.Name).ToArray());
+        }
+    }
+}
